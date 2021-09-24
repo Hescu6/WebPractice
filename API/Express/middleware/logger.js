5f68e503f8f87dae22ee1b1ef622ec0f6b15cb1a
Hildebrando <tususpop@gmail.com>
2021-09-24 13:32:23 -0400

    rearranged

--- API/Express/middleware/logger.js
+++ API/Express/middleware/logger.js
@@ -1,4 +1,6 @@
-//middleware used to log
+const moment = require("moment");
+
+//middleware used to log requests
 const logger = (req, res, next) => {
   console.log(
     `${req.protocol}://${req.get("host")}${
@@ -8,4 +10,4 @@ const logger = (req, res, next) => {
   next();
 };
 
-modules.exports = logger;
+module.exports = logger;
