c74b6ac8a692de9c52af589029a427b740deb91d
Hildebrando <tususpop@gmail.com>
2021-09-24 13:41:50 -0400

    added delete member route

--- API/Express/routes/api/members.js
+++ API/Express/routes/api/members.js
@@ -3,10 +3,8 @@ const uuid = require("uuid");
 const router = express.Router();
 const members = require("../../Members");
 
-
 //get all members in Members.js
 router.get("/", (req, res) => res.json(members));
-
 
 //get a single member
 router.get("/:id", (req, res) => {
@@ -18,40 +16,55 @@ router.get("/:id", (req, res) => {
   }
 });
 
-
 //Create member
-router.post('/', (req, res) => {
-    const newMember = {
-        id: uuid.v4(),
-        name: req.body.name,
-        email: req.body.email,
-        status: 'active'
-    }
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  };
 
-    if (!newMember.name || !newMember.email) {
-        return res.status(400).json({ msg: "Please include name and email"});
-    }
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include name and email" });
+  }
 
-    members.push(newMember);
-    res.json(members);
+  members.push(newMember);
+  res.json(members);
 });
 
 // Put request - update member
 router.put("/:id", (req, res) => {
-    const found = members.some((member) => member.id === parseInt(req.params.id));
-    if (found) {
-      const updateMember = req.body;
-      members.forEach( member => {
-          if(member.id === parseInt(req.params.id)){
-              member.name = updateMember.name ? updateMember.name : member.name;
-              member.email = updateMember.email ? updateMember.email : member.email;
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    const updateMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updateMember.name ? updateMember.name : member.name;
+        member.email = updateMember.email ? updateMember.email : member.email;
 
-              res.json({ msg: `Member updated`, member});
-          }
-      });
-    } else {
-      res.status(400).json({ msg: `Member Id ${req.params.id} not found` });
-    }
-  });
+        res.json({ msg: `Member updated`, member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `Member Id ${req.params.id} not found` });
+  }
+});
+
+//DELETE member
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      )
+    });
+  } else {
+    res.status(400).json({ msg: `Member Id ${req.params.id} not found` });
+  }
+});
 
 module.exports = router;
