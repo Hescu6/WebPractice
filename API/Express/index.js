6a7a91a5f99be30b705e9d24aab34d795cfbbda9
Hildebrando <tususpop@gmail.com>
2021-09-24 14:08:18 -0400

    add members to template

--- API/Express/index.js
+++ API/Express/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const path = require("path");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
-
-
+const members = require('./Members');
 const app = express();
 
 // // manually set every route (not ideal)
@@ -12,19 +11,30 @@ const app = express();
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // })
 
-// init middleware
+// init logger middleware
 app.use(logger);
+
+//handlebars middleware to render templates
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Homepage Route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members: members
+  })
+);
 
 // Set static folder (best way)
 app.use(express.static(path.join(__dirname, "public")));
 
 //members API route
-app.use('/api/members', require('./routes/api/members'));
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORTEXERCISE || 5000;
 
