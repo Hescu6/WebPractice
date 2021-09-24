ee56cd5b2ec4454ebb1bf4b497c404e5c45d930f
Hildebrando <tususpop@gmail.com>
2021-09-24 13:25:01 -0400

    added routes - get, put, and post members

---
+++ API/Express/routes/api/members.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const uuid = require("uuid");
+const router = express.Router();
+const members = require("../../Members");
+
+
+//get all members in Members.js
+router.get("/", (req, res) => res.json(members));
+
+
+//get a single member
+router.get("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `Member Id ${req.params.id} not found` });
+  }
+});
+
+
+//Create member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: "Please include name and email"});
+    }
+
+    members.push(newMember);
+    res.json(members);
+});
+
+// Put request - update member
+router.put("/:id", (req, res) => {
+    const found = members.some((member) => member.id === parseInt(req.params.id));
+    if (found) {
+      const updateMember = req.body;
+      members.forEach( member => {
+          if(member.id === parseInt(req.params.id)){
+              member.name = updateMember.name ? updateMember.name : member.name;
+              member.email = updateMember.email ? updateMember.email : member.email;
+
+              res.json({ msg: `Member updated`, member});
+          }
+      });
+    } else {
+      res.status(400).json({ msg: `Member Id ${req.params.id} not found` });
+    }
+  });
+
+module.exports = router;
